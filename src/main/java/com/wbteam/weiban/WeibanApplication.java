b4297bf076c298d083471f8e228a14a8949b1253
saligialin <3027862603@qq.com>
2021-04-24 00:14:45 +0800

    Init Project

---
+++ src/main/java/com/wbteam/weiban/WeibanApplication.java
@@ -0,0 +1,13 @@
+package com.wbteam.weiban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeibanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeibanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wbteam/weiban/WeibanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbteam.weiban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeibanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
