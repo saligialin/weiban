9014bf9cb8923a9a399b9b774ed4dc288a1729dd
saligialin <3027862603@qq.com>
2021-04-25 00:08:10 +0800

    配置mapper扫描

--- src/main/java/com/wbteam/weiban/WeibanApplication.java
+++ src/main/java/com/wbteam/weiban/WeibanApplication.java
@@ -1,9 +1,11 @@
 package com.wbteam.weiban;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wbteam.weiban.mapper")
 public class WeibanApplication {
 
     public static void main(String[] args) {
