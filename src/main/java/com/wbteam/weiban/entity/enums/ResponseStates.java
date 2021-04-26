3c45b1238fd0bb7541375d149e4c5c77c0c921b7
saligialin <3027862603@qq.com>
2021-04-26 16:28:09 +0800

    返回状态更新

--- src/main/java/com/wbteam/weiban/entity/enums/ResponseStates.java
+++ src/main/java/com/wbteam/weiban/entity/enums/ResponseStates.java
@@ -2,14 +2,16 @@ package com.wbteam.weiban.entity.enums;
 
 public enum ResponseStates {
     SUCCESS("操作成功",200),
-    EXISTED_ACCOUNT("用户已存在", 601),
+    ERROR("错误",100),
+    EXISTED_ACCOUNT("用户已存在", 101),
     UNKNOWN_ACCOUNT("用户不存在", 602),
-    PASSWORD_INCORRECT("密码错误", 603),
-    LOCKED_ACCOUNT("此账户已被禁用",604),
-    AUTHORITY_INSUFFICIENT("用户无此权限",605),
-    MESSAGE_CODE_INCORRECT("短信验证码错误",606),
-    IMAGE_FORMAT_ERROR("图片格式错误",607),
-    ERROR("错误",100);
+    PASSWORD_INCORRECT("密码错误", 103),
+    MESSAGE_CODE_INCORRECT("短信验证码错误",104),
+    TOKEN_NOT_PROVIDE("未提供Token",105),
+    TOKEN_IS_ERROR("Token错误",106),
+    TOKEN_IS_EXPIRED("Token过期",107),
+    ROLE_IS_ERROR("用户角色选择错误",108)
+    ;
 
     private final String message;
     private final int value;
