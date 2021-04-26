45d953fc2ef67cfd6b08a7fda79294da79c958f7
saligialin <3027862603@qq.com>
2021-04-26 22:39:38 +0800

    BUG修复

--- src/main/java/com/wbteam/weiban/service/impl/CarerServiceImpl.java
+++ src/main/java/com/wbteam/weiban/service/impl/CarerServiceImpl.java
@@ -21,7 +21,7 @@ public class CarerServiceImpl implements CarerService {
         log.info("开始向数据插入新的护工用户");
         try {
             carer.setId(UUID.randomUUID().toString());
-            return carerMapper.insert(carer);
+            return carerMapper.insertCarer(carer);
         } catch (Exception e) {
             log.info("向数据插入新的护工用户异常：");
             log.info(e.toString());
@@ -33,7 +33,7 @@ public class CarerServiceImpl implements CarerService {
     public int deleteById(String id) {
         log.info("开始删除护工用户"+id);
         try {
-            return carerMapper.deleteById(id);
+            return carerMapper.deleteCarerById(id);
         } catch (Exception e) {
             log.info("删除护工用户"+id+"异常：");
             log.info(e.toString());
@@ -45,7 +45,7 @@ public class CarerServiceImpl implements CarerService {
     public int updateById(Carer carer) {
         log.info("开始更新护工用户"+carer.getId()+"的信息");
         try {
-            return carerMapper.updateById(carer);
+            return carerMapper.updateCarerById(carer);
         } catch (Exception e) {
             log.info("更新护工用户"+carer.getId()+"的信息异常");
             log.info(e.toString());
@@ -57,7 +57,7 @@ public class CarerServiceImpl implements CarerService {
     public Carer selectById(String id) {
         log.info("开始获取护工用户"+id+"的信息");
         try {
-            return carerMapper.selectById(id);
+            return carerMapper.getCarerById(id);
         } catch (Exception e) {
             log.info("获取护工用户"+id+"的信息异常：");
             log.info(e.toString());
