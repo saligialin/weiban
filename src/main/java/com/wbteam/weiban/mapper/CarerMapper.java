e91464b6502a67dd8684aef8cde35d628e8c3c51
saligialin <3027862603@qq.com>
2021-04-26 22:38:50 +0800

    功能完善

--- src/main/java/com/wbteam/weiban/mapper/CarerMapper.java
+++ src/main/java/com/wbteam/weiban/mapper/CarerMapper.java
@@ -1,12 +1,19 @@
 package com.wbteam.weiban.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wbteam.weiban.entity.Carer;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface CarerMapper extends BaseMapper<Carer> {
+public interface CarerMapper{
+    int insertCarer(Carer carer);
+
+    Carer getCarerById(String id);
+
     Carer getCarerByTel(String tel);
+
+    int updateCarerById(Carer carer);
+
+    int deleteCarerById(String id);
 
     boolean changePassword(Carer carer);
 }
