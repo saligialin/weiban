3ebc6722109b6575caf56965e661d5dc60d99935
saligialin <3027862603@qq.com>
2021-04-27 20:17:53 +0800

    Passage Mapper

--- src/main/java/com/wbteam/weiban/mapper/PassageMapper.java
+++ src/main/java/com/wbteam/weiban/mapper/PassageMapper.java
@@ -1,9 +1,24 @@
 package com.wbteam.weiban.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wbteam.weiban.entity.Passage;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
-public interface PassageMapper extends BaseMapper<Passage> {
+public interface PassageMapper{
+
+    int insertPassage(Passage passage);
+
+    int deletePassageById(String id);
+
+    int updatePassage(Passage passage);
+
+    Passage getPassageById(String id);
+
+    List<Passage> getPassage(int currPage, int pageSize);
+
+    List<Passage> getPassageByAuthorId(String authorId);
+
+    boolean changeSocre(String id, double score);
 }
