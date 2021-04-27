3860b6e39e976b74d99a0c06e4f17e365039c8fe
saligialin <3027862603@qq.com>
2021-04-27 17:51:03 +0800

    Memo Mapper

--- src/main/java/com/wbteam/weiban/mapper/MemoMapper.java
+++ src/main/java/com/wbteam/weiban/mapper/MemoMapper.java
@@ -1,9 +1,21 @@
 package com.wbteam.weiban.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wbteam.weiban.entity.Memo;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
-public interface MemoMapper extends BaseMapper<Memo> {
+public interface MemoMapper{
+    int insertMemo(Memo memo);
+
+    int updateMemo(Memo memo);
+
+    int deleteMemo(String id);
+
+    Memo getMemoById(String id);
+
+    List<Memo> getListByElderId(String elderId);
+
+    List<Memo> getListByYouthId(String youthId);
 }
