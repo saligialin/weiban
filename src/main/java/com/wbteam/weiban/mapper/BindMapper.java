011c3b3b40253426278a4f3427e35775514e120c
saligialin <3027862603@qq.com>
2021-04-27 13:09:52 +0800

    功能完善

--- src/main/java/com/wbteam/weiban/mapper/BindMapper.java
+++ src/main/java/com/wbteam/weiban/mapper/BindMapper.java
@@ -1,9 +1,25 @@
 package com.wbteam.weiban.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wbteam.weiban.entity.Bind;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
-public interface BindMapper extends BaseMapper<Bind> {
+public interface BindMapper{
+
+    int insertBind(Bind bind);
+
+    List<Bind> getBindByElderId(String elderId);
+
+    List<Bind> getBindByYouthId(String youthId);
+
+    int updateBind(Bind bind);
+
+    int deleteBind(String id);
+
+    List<Bind> getUnconfirmedBindingForElder(String elderId);
+
+    List<Bind> getUnconfirmedBindingForYouth(String youthId);
+
 }
